6c69f838217cc1cc9d3ac6e8835b4069d80bc4db
Oren <oren.sokolowsky@gmail.com>
2017-08-02 08:51:01 +0300

    Clean courtroomabstract.sol

--- contracts/abstracts/courtroomAbstract.sol
+++ contracts/abstracts/courtroomAbstract.sol
@@ -5,15 +5,6 @@ import "./owned.sol";
 
 contract SwearGameAbstract is Owned {
 
-    /// @notice register - register a player to the game
-    ///
-    /// The function will throw if the player is already register or there is not
-    /// enough deposit in the contract to ensure the player could be compensated for the
-    /// case of a valid case.
-    /// @param _player  - the player address
-    /// @return bool registered - true for success registration.
-    //function register(address _player) onlyOwner public returns (bool);
-
     /// @notice leaveGame - dismiss a player from the game (unregister)
     /// allow only plaintiff which do not have openCases on it name to leave game
     /// @param _player  - the player address
@@ -39,20 +30,8 @@ contract SwearGameAbstract is Owned {
     /// @return bool - true for successful operation.
     function trial(bytes32 id) public returns (bool);
 
-    /*function deposit(uint epochs) payable returns (bool);
-
-    function collectDeposit() external returns (bool);
-
-    function isRegister(address player) returns (bool);*/
-
     event Decision(string decide);
-    /*event DepositStaked(uint depositAmount, uint deposit);
-    event Compensate(address recipient, uint reward);
-    event NewPlayer(address playerId);
-    event PlayerLeftGame(address playerId);*/
     event NewCaseOpened(bytes32 id, address plaintiff);
     event NewEvidenceSubmitted(bytes32 id, address plaintiff);
     event CaseResolved(bytes32 id, address plaintiff, uint reward,uint8 status);
-    /*event Payment(address from,address to ,uint256 value);
-    event AdditionalDepositRequired(uint256 deposit);*/
 }
