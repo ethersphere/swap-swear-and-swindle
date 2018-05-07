a73b52fa8b72cf0c7ce12d3d71339bc567b95223
Ralph Pichler <pichler.ralph@gmail.com>
2018-05-07 11:50:23 +0200

    add sample trial

---
+++ contracts/abstracts/AbstractRules.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.0;
+import "./AbstractWitness.sol";
+
+contract AbstractRules {
+
+  uint8 constant TRIAL_STATUS_UNCHALLENGED = 0;
+  uint8 constant TRIAL_STATUS_GUILTY = 1;
+  uint8 constant TRIAL_STATUS_NOT_GUILTY = 2;
+
+  function nextStatus(AbstractWitness.TestimonyStatus witnessStatus,uint8 trialStatus) public view returns (uint8 status);
+  function getWitness(uint8 trialStatus) public view returns (address witness, uint expiry);
+  function getInitialStatus() public view returns (uint8 status);
+
+  /* get minimal deposit for service */
+  function getDeposit() public view returns (uint deposit);
+
+  /* get minimal epoch for service */
+  function getEpoch() public view returns (uint epoch);
+
+}
---
+++ contracts/test/OracleTrial.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.19;
+import "../abstracts/AbstractRules.sol";
+import "./OracleWitness.sol";
+
+contract SampleTrial is AbstractRules {
+  uint8 constant TRIAL_STATUS_WITNESS_1 = 3;
+  uint8 constant TRIAL_STATUS_WITNESS_2 = 4;
+
+  address public witness1 = new OracleWitness();
+  address public witness2 = new OracleWitness();
+
+  function nextStatus(AbstractWitness.TestimonyStatus witnessStatus, uint8 trialStatus)
+  public view returns (uint8 status) {
+    require(witnessStatus == AbstractWitness.TestimonyStatus.VALID
+      || witnessStatus == AbstractWitness.TestimonyStatus.INVALID);
+
+    if(trialStatus == TRIAL_STATUS_WITNESS_1) {
+      if(witnessStatus == AbstractWitness.TestimonyStatus.VALID) return TRIAL_STATUS_WITNESS_2;
+      else return TRIAL_STATUS_NOT_GUILTY;
+    } else if(trialStatus == TRIAL_STATUS_WITNESS_2){
+      if(witnessStatus == AbstractWitness.TestimonyStatus.VALID) return TRIAL_STATUS_GUILTY;
+      else return TRIAL_STATUS_NOT_GUILTY;
+    } else revert();
+  }
+
+  function getWitness(uint8 trialStatus)
+  public view returns (address, uint) {
+    if(trialStatus == TRIAL_STATUS_WITNESS_1) return  (witness1, 2 days);
+    if(trialStatus == TRIAL_STATUS_WITNESS_2) return  (witness2, 2 days);
+    revert();
+  }
+
+  function getInitialStatus()
+  public view returns (uint8 status) {
+    return TRIAL_STATUS_WITNESS_1;
+  }
+
+  /* get minimal deposit for service */
+  function getDeposit() public view returns (uint deposit) {
+    return 10 ether;
+  }
+
+  /* get minimal epoch for service */
+  function getEpoch() public view returns (uint epoch) {
+    return 30 days;
+  }
+}
