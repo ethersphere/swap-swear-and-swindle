24afee9405a2856e3bf4fc74f2ce5ca69242d765
Ralph Pichler <pichler.ralph@gmail.com>
2018-05-11 14:09:27 +0200

    deploy SimpleTrial in tests instead of migrations

--- migrations/3_deploy_swindle.js
+++ migrations/3_deploy_swindle.js
@@ -1,9 +1,7 @@
 var Swindle = artifacts.require("./Swindle.sol");
 var OracleTrial = artifacts.require("./OracleTrial.sol");
-var HashWitness = artifacts.require("./HashWitness.sol")
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Swindle);
   deployer.deploy(OracleTrial);
-  deployer.deploy(HashWitness);
 };
--- migrations/5_deploy_hash_trial.js
+++
@@ -1,6 +0,0 @@
-var HashWitness = artifacts.require("./HashWitness.sol");
-var SimpleTrial = artifacts.require("./SimpleTrial.sol");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(SimpleTrial, HashWitness.address);
-};
