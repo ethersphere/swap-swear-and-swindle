0b335abaf1410b77073aa3e5500ed8ce4d67acfc
Ralph Pichler <pichler.ralph@gmail.com>
2018-05-11 10:13:55 +0200

    use pure where possible

--- contracts/SW3Utils.sol
+++ contracts/SW3Utils.sol
@@ -5,11 +5,13 @@ import "./abstracts/AbstractWitness.sol";
 
 contract SW3Utils {
 
-  function chequeHash(address swap, address beneficiary, uint serial, uint amount) public view returns (bytes32) {
+  function chequeHash(address swap, address beneficiary, uint serial, uint amount)
+  public pure returns (bytes32) {
     return keccak256(abi.encodePacked(swap, serial, beneficiary, amount));
   }
 
-  function noteHash(address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark) public view returns (bytes32) {
+  function noteHash(address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark)
+  public pure returns (bytes32) {
     return keccak256(encodeNote(swap, beneficiary, index, amount, witness, validFrom, validUntil, remark));
   }
 
@@ -17,12 +19,13 @@ contract SW3Utils {
     return keccak256(abi.encodePacked(noteId, swapBalance, serial));
   }
 
-  function encodeNote(address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark) public view returns (bytes) {
+  function encodeNote(address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark)
+  public pure returns (bytes) {
     return abi.encodePacked(swap, index, beneficiary, amount, witness, validFrom, validUntil, remark);
   }
 
   function decodeNote(bytes note)
-  public view returns (address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark) {
+  public pure returns (address swap, address beneficiary, uint index, uint amount, address witness, uint validFrom, uint validUntil, bytes32 remark) {
     uint divisor = 2**96;
     assembly {
       swap := div(mload(add(note, 32)), divisor)
@@ -47,7 +50,10 @@ contract SW3Utils {
   }
 
   function recoverSignature(bytes32 hash, bytes sig) internal pure returns (address) {
-    var (r, s, v) = decodeSignature(sig);
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    (r, s, v) = decodeSignature(sig);
     return ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)), v, r, s);
   }
 
