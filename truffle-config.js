6ec8f2e89460828fb1845c38453548cbdb02ee31
Ralph Pichler <pichler.ralph@gmail.com>
2019-11-18 19:06:40 +0100

    version bump to 0.2.0 (#96)
    * bump to 0.2.0
    * bump solidity and go-ethereum version

--- truffle-config.js
+++ truffle-config.js
@@ -73,7 +73,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.11",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.13",    // Fetch exact version from solc-bin (default: truffle's version)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
