97485221ac6807cd326673291356df1ddcb906b7
Ralph Pichler <pichler.ralph@gmail.com>
2021-03-17 16:34:00 +0100

    migrate to hardhat (#121)

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-truffle5");
+require("solidity-coverage")
+
+
+// Define mnemonic for accounts.
+let mnemonic = process.env.MNEMONIC;
+if (!mnemonic) {
+  // NOTE: this fallback is for development only!
+  // When using other networks, set the secret in .env.
+  // DO NOT commit or share your mnemonic with others!
+  mnemonic = 'test test test test test test test test test test test test';
+}
+
+const accounts = { mnemonic };
+
+// Config for hardhat.
+module.exports = {
+  solidity: { version: '0.6.12' },  
+  networks: {
+    hardhat: {
+      accounts,
+    },
+    localhost: {
+      url: 'http://localhost:8545',
+      accounts,
+    },
+    staging: {
+      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_TOKEN,
+      accounts,
+    },
+  },
+  paths: {
+    sources: 'contracts',
+  },
+};
