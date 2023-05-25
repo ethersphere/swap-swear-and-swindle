deab819d37b427d60581dd6f742f454e6dd22015
marko.b <adriadrop@gmail.com>
2023-05-25 12:40:05 +0200

    make final adjustments for deployment

--- deploy/000_deploy.js
+++ deploy/000_deploy.js
@@ -2,26 +2,27 @@ const { getNamedAccounts, deployments, network, run } = require("hardhat");
 const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
-    const waitBlockConfirmations = 6;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    log("----------------------------------------------------");
-    const arguments = ["0xa66be4A7De4DfA5478Cb2308469D90115C45aA23"];
-    const factory = await deploy("SimpleSwapFactory", {
-        from: deployer,
-        args: arguments,
-        log: true,
-        waitConfirmations: waitBlockConfirmations,
-    });
+  const waitBlockConfirmations = network.name != "testnet" ? 1 : 6;
 
-    log("Factory deployed at address " + factory.address);
+  log("----------------------------------------------------");
+  const arguments = ["0xa66be4A7De4DfA5478Cb2308469D90115C45aA23"];
+  const factory = await deploy("SimpleSwapFactory", {
+    from: deployer,
+    args: arguments,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
 
-        // Verify the deployment
-    if ((network.name == "testnet") && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying...");
-        await verify(nft.address, arguments);
-    }
+  log("Factory deployed at address " + factory.address);
+
+  // Verify the deployment
+  if (network.name == "testnet" && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(factory.address, arguments);
+  }
 };
 
 module.exports.tags = ["all", "factory"];
