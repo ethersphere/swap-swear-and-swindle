df715c85784758a09eb027e1c8df333ba936a2fd
marko.b <adriadrop@gmail.com>
2023-05-25 11:57:02 +0200

    updates to tooling and config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomiclabs/hardhat-truffle5");
 require("solidity-coverage")
+require('dotenv/config');
 
+const PRIVATE_RPC_MAINNET = !process.env.PRIVATE_RPC_MAINNET ? undefined : process.env.PRIVATE_RPC_MAINNET;
+const PRIVATE_RPC_TESTNET = !process.env.PRIVATE_RPC_TESTNET ? undefined : process.env.PRIVATE_RPC_TESTNET;
 
 const walletSecret = process.env.WALLET_SECRET === undefined ? 'undefined' : process.env.WALLET_SECRET;
 if (walletSecret === 'undefined') {
   console.log('Please set your WALLET_SECRET in a .env file');
 }
 const accounts = walletSecret.length === 64 ? [walletSecret] : { mnemonic: walletSecret };
-
 
 // Config for hardhat.
 module.exports = {
@@ -21,15 +23,20 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      accounts,
     },
     localhost: {
       url: 'http://localhost:8545',
       accounts,
     },
-    staging: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_TOKEN,
+    testnet: {
+      url: PRIVATE_RPC_TESTNET ? PRIVATE_RPC_TESTNET : 'https://rpc2.sepolia.org',
       accounts,
+      chainId: 11155111,
+    },
+    mainnet: {
+      url: PRIVATE_RPC_MAINNET ? PRIVATE_RPC_MAINNET : 'https://rpc.gnosischain.com',
+      accounts,
+      chainId: 100,
     },
   },
   paths: {
