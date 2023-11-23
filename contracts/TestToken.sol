896b7d82f83b37edbc5e797e29a09108e735a000
Mark.B <adriadrop@gmail.com>
2023-11-23 22:53:34 +0100

    use current status

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity =0.7.6;
+pragma solidity =0.8.19;
 
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract TestToken is ERC20PresetMinterPauser {
 
