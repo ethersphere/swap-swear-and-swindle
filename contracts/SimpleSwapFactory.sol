9ed3078efbc7b6e5834c9ea9c1ad9ab436868331
Marko <adriadrop@gmail.com>
2023-11-29 16:28:23 +0100

    try to migrate with this function

--- contracts/SimpleSwapFactory.sol
+++ contracts/SimpleSwapFactory.sol
@@ -10,37 +10,73 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 @notice This contract deploys SimpleSwap contracts
 */
 contract SimpleSwapFactory {
+    /* event fired on every new SimpleSwap deployment */
+    event SimpleSwapDeployed(address contractAddress);
 
-  /* event fired on every new SimpleSwap deployment */
-  event SimpleSwapDeployed(address contractAddress);
+    /* mapping to keep track of which contracts were deployed by this factory */
+    mapping(address => bool) public deployedContracts;
 
-  /* mapping to keep track of which contracts were deployed by this factory */
-  mapping (address => bool) public deployedContracts;
+    /* address of the ERC20-token, to be used by the to-be-deployed chequebooks */
+    address public ERC20Address;
+    /* address of the code contract from which all chequebooks are cloned */
+    address public master;
 
-  /* address of the ERC20-token, to be used by the to-be-deployed chequebooks */
-  address public ERC20Address;
-  /* address of the code contract from which all chequebooks are cloned */
-  address public master;
+    constructor(address _ERC20Address) {
+        ERC20Address = _ERC20Address;
+        ERC20SimpleSwap _master = new ERC20SimpleSwap();
+        // set the issuer of the master contract to prevent misuse
+        _master.init(address(1), address(0), 0);
+        master = address(_master);
+    }
 
-  constructor(address _ERC20Address) {
-    ERC20Address = _ERC20Address;
-    ERC20SimpleSwap _master = new ERC20SimpleSwap();
-    // set the issuer of the master contract to prevent misuse
-    _master.init(address(1), address(0), 0);
-    master = address(_master);
-  }
-  /**
+    /**
   @notice creates a clone of the master SimpleSwap contract
   @param issuer the issuer of cheques for the new chequebook
   @param defaultHardDepositTimeoutDuration duration in seconds which by default will be used to reduce hardDeposit allocations
   @param salt salt to include in create2 to enable the same address to deploy multiple chequebooks
   */
-  function deploySimpleSwap(address issuer, uint defaultHardDepositTimeoutDuration, bytes32 salt)
-  public returns (address) {    
-    address contractAddress = Clones.cloneDeterministic(master, keccak256(abi.encode(msg.sender, salt)));
-    ERC20SimpleSwap(contractAddress).init(issuer, ERC20Address, defaultHardDepositTimeoutDuration);
-    deployedContracts[contractAddress] = true;
-    emit SimpleSwapDeployed(contractAddress);
-    return contractAddress;
-  }
+    function deploySimpleSwap(
+        address issuer,
+        uint defaultHardDepositTimeoutDuration,
+        bytes32 salt
+    ) public returns (address) {
+        address contractAddress = Clones.cloneDeterministic(
+            master,
+            keccak256(abi.encode(msg.sender, salt))
+        );
+        ERC20SimpleSwap(contractAddress).init(
+            issuer,
+            ERC20Address,
+            defaultHardDepositTimeoutDuration
+        );
+        deployedContracts[contractAddress] = true;
+        emit SimpleSwapDeployed(contractAddress);
+        return contractAddress;
+    }
+
+    /**
+  @notice migrates a clone of a master SimpleSwap contract
+  @param issuer the issuer of cheques for the new chequebook
+  @param defaultHardDepositTimeoutDuration duration in seconds which by default will be used to reduce hardDeposit allocations
+  @param salt salt to include in create2 to enable the same address to deploy multiple chequebooks
+  */
+    function migrateSimpleSwap(
+        address issuer,
+        uint defaultHardDepositTimeoutDuration,
+        bytes32 salt,
+        address _master
+    ) public returns (address) {
+        address contractAddress = Clones.cloneDeterministic(
+            _master,
+            keccak256(abi.encode(issuer, salt))
+        );
+        ERC20SimpleSwap(contractAddress).init(
+            issuer,
+            ERC20Address,
+            defaultHardDepositTimeoutDuration
+        );
+        deployedContracts[contractAddress] = true;
+        emit SimpleSwapDeployed(contractAddress);
+        return contractAddress;
+    }
 }
