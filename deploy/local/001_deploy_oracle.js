8f61a5e78e4fdac1fec205415768635fef483e92
Marko <adriadrop@gmail.com>
2024-03-06 14:33:54 +0100

    add oracle deployments to repo

---
+++ deploy/local/001_deploy_oracle.js
@@ -0,0 +1,24 @@
+const { network } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = 1;
+
+  log("----------------------------------------------------");
+  const args = [100, 200];
+
+  // Deploy the PriceOracle contract
+  const oracle = await deploy('SwapPriceOracle', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
+
+  // Log the address at which the Oracle is deployed
+  console.log('Oracle deployed at address ' + oracle.address);
+
+};
+
+module.exports.tags = ["factory"];
---
+++ deploy/test/001_deploy_oracle.js
@@ -0,0 +1,27 @@
+const { getNamedAccounts, deployments, network, run } = require("hardhat");
+const { verify } = require("../../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const args = [100000, 100];
+
+  // Deploy the PriceOracle contract
+  const oracle = await deploy('SwapPriceOracle', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: 6,
+  });
+
+  // Log the address at which the Oracle is deployed
+  console.log('Oracle deployed at address ' + oracle.address)
+
+  // Verify the deployment
+  if (network.name == "testnet" && process.env.TESTNET_ETHERSCAN_KEY) {
+    log("Verifying...");
+    await verify(oracle.address, arguments);
+  }
+};
+
+module.exports.tags = ["factory"];
