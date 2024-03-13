c2ef4caa885b7b588d88eebc19c60c976489f6f1
Marko <adriadrop@gmail.com>
2024-03-13 16:02:40 +0100

    make condition for cluster and local token versions

--- deploy/local/000_deploy_factory.ts
+++ deploy/local/000_deploy_factory.ts
@@ -1,20 +1,32 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network, ethers }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const waitBlockConfirmations = network.name != 'testnet' ? 1 : 6;
 
   log('----------------------------------------------------');
 
-  const token = await deploy('TestToken', {
-    from: deployer,
-    log: true,
-  });
+  // Check if this is run on Cluster and is already using deployed token from Storage Incentive
+  const deployedToken = "0x942C6684eB9874C63d4ed26Ab0623F951D253081";
+  const code = await ethers.provider.getCode(deployedToken);
+  let tokenAddress;
 
-  log(`Token deployed at address ${token.address}`);
+  // If there's code, then there's a contract deployed
+  if (code !== '0x') {
+    console.log('A contract is deployed at this address.');
+    tokenAddress = deployedToken;
+  } else {
+    const token = await deploy('TestToken', {
+      from: deployer,
+      log: true,
+    });
+    tokenAddress = token.address;
+  }
 
-  const deployArgs: string[] = [token.address];
+  log(`Token deployed at address ${tokenAddress}`);
+
+  const deployArgs: string[] = [tokenAddress];
   const factory = await deploy('SimpleSwapFactory', {
     from: deployer,
     args: deployArgs,
