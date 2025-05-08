a191c13588eff8ca7e81a6228925f3503fb27e4f
Cardinal <adriadrop@gmail.com>
2025-05-08 18:39:43 +0200

    add mainet deployment

---
+++ deploy/main/001_deploy_oracle.ts
@@ -0,0 +1,28 @@
+import verify from '../../utils/verify';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const deployArgs = [100000, 100];
+
+  // Deploy the PriceOracle contract
+  const oracle = await deploy('PriceOracle', {
+    from: deployer,
+    args: deployArgs,
+    log: true,
+    waitConfirmations: 6,
+  });
+
+  // Log the address at which the Oracle is deployed
+  console.log('Oracle deployed at address ' + oracle.address);
+
+  // Verify the deployment
+  if (network.name == 'mainnet' && process.env.MAINNET_ETHERSCAN_KEY) {
+    log('Verifying...');
+    await verify(oracle.address, deployArgs);
+  }
+};
+
+func.tags = ['oracle'];
+export default func;
